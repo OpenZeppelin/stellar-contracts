62a8ecdd162df53baa550166bdb9066d91dabd07
Ozgun Ozerk <ozgun@subspace.network>
2025-02-04 09:20:20 +0300

    reuse the duplicated code across macros

--- contracts/utils/pausable-macros/src/lib.rs
+++ contracts/utils/pausable-macros/src/lib.rs
@@ -1,8 +1,6 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, ItemFn};
 
-use crate::helper::check_env_arg;
+use crate::helper::generate_pause_check;
 
 mod helper;
 
@@ -28,32 +26,7 @@ mod helper;
 /// ```
 #[proc_macro_attribute]
 pub fn when_not_paused(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input_fn = parse_macro_input!(item as ItemFn);
-    let (env_ident, is_ref) = check_env_arg(&input_fn);
-
-    let fn_vis = &input_fn.vis;
-    let fn_sig = &input_fn.sig;
-    let fn_block = &input_fn.block;
-    // Filter out the current macro from attributes
-    let fn_attrs: Vec<_> =
-        input_fn.attrs.iter().filter(|attr| !attr.path().is_ident("when_not_paused")).collect();
-
-    let env_arg = if is_ref {
-        quote! { #env_ident }
-    } else {
-        quote! { &#env_ident }
-    };
-
-    let output = quote! {
-        #(#fn_attrs)* // retain other macros
-        #fn_vis #fn_sig {
-            openzeppelin_pausable::when_not_paused(#env_arg);
-
-            #fn_block
-        }
-    };
-
-    output.into()
+    generate_pause_check(item, "when_not_paused", "when_not_paused")
 }
 
 /// Adds a pause check at the beginning of the function that ensures the
@@ -78,30 +51,5 @@ pub fn when_not_paused(_attr: TokenStream, item: TokenStream) -> TokenStream {
 /// ```
 #[proc_macro_attribute]
 pub fn when_paused(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input_fn = parse_macro_input!(item as ItemFn);
-    let (env_ident, is_ref) = check_env_arg(&input_fn);
-
-    let fn_vis = &input_fn.vis;
-    let fn_sig = &input_fn.sig;
-    let fn_block = &input_fn.block;
-    // Filter out the current macro from attributes
-    let fn_attrs: Vec<_> =
-        input_fn.attrs.iter().filter(|attr| !attr.path().is_ident("when_paused")).collect();
-
-    let env_arg = if is_ref {
-        quote! { #env_ident }
-    } else {
-        quote! { &#env_ident }
-    };
-
-    let output = quote! {
-        #(#fn_attrs)* // retain other macros
-        #fn_vis #fn_sig {
-            openzeppelin_pausable::when_paused(#env_arg);
-
-            #fn_block
-        }
-    };
-
-    output.into()
+    generate_pause_check(item, "when_paused", "when_paused")
 }
