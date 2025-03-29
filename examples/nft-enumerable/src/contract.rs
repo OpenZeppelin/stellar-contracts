be0adb1f019c8f80a633dbf7e72f2cab1a2e381a
Ozgun Ozerk <ozgun@subspace.network>
2025-03-29 17:47:10 +0300

    mod approach

--- examples/nft-enumerable/src/contract.rs
+++ examples/nft-enumerable/src/contract.rs
@@ -9,9 +9,8 @@
 
 use soroban_sdk::{contract, contractimpl, Address, Env, String};
 use stellar_non_fungible::{
-    self as non_fungible,
-    enumerable::{overrides::Enumerable, NonFungibleEnumerable},
-    Balance, ContractOverrides, NonFungibleToken, TokenId,
+    enumerable::{Enumerable, NonFungibleEnumerable},
+    Balance, Base, ContractOverrides, NonFungibleToken, TokenId,
 };
 
 #[contract]
@@ -20,7 +19,7 @@ pub struct ExampleContract;
 #[contractimpl]
 impl ExampleContract {
     pub fn __constructor(e: &Env) {
-        non_fungible::set_metadata(
+        Base::set_metadata(
             e,
             String::from_str(e, "www.mytoken.com"),
             String::from_str(e, "My Token"),
@@ -34,7 +33,7 @@ impl NonFungibleToken for ExampleContract {
     type ContractType = Enumerable;
 
     fn balance(e: &Env, owner: Address) -> Balance {
-        non_fungible::balance(e, &owner)
+        Self::ContractType::balance(e, owner)
     }
 
     fn owner_of(e: &Env, token_id: TokenId) -> Address {
@@ -42,11 +41,11 @@ impl NonFungibleToken for ExampleContract {
     }
 
     fn transfer(e: &Env, from: Address, to: Address, token_id: TokenId) {
-        Self::ContractType::transfer(e, from, to, token_id);
+        Self::ContractType::transfer(e, &from, &to, token_id);
     }
 
     fn transfer_from(e: &Env, spender: Address, from: Address, to: Address, token_id: TokenId) {
-        Self::ContractType::transfer_from(e, spender, from, to, token_id);
+        Self::ContractType::transfer_from(e, &spender, &from, &to, token_id);
     }
 
     fn approve(
@@ -60,53 +59,53 @@ impl NonFungibleToken for ExampleContract {
     }
 
     fn approve_for_all(e: &Env, owner: Address, operator: Address, live_until_ledger: u32) {
-        non_fungible::approve_for_all(e, &owner, &operator, live_until_ledger);
+        Self::ContractType::approve_for_all(e, owner, operator, live_until_ledger);
     }
 
     fn get_approved(e: &Env, token_id: TokenId) -> Option<Address> {
-        non_fungible::get_approved(e, token_id)
+        Self::ContractType::get_approved(e, token_id)
     }
 
     fn is_approved_for_all(e: &Env, owner: Address, operator: Address) -> bool {
-        non_fungible::is_approved_for_all(e, &owner, &operator)
+        Self::ContractType::is_approved_for_all(e, owner, operator)
     }
 
     fn name(e: &Env) -> String {
-        non_fungible::name(e)
+        Self::ContractType::name(e)
     }
 
     fn symbol(e: &Env) -> String {
-        non_fungible::symbol(e)
+        Self::ContractType::symbol(e)
     }
 
     fn token_uri(e: &Env, token_id: TokenId) -> String {
-        non_fungible::token_uri(e, token_id)
+        Self::ContractType::token_uri(e, token_id)
     }
 }
 
 #[contractimpl]
 impl NonFungibleEnumerable for ExampleContract {
     fn total_supply(e: &Env) -> Balance {
-        non_fungible::enumerable::storage::total_supply(e)
+        Enumerable::total_supply(e)
     }
 
     fn get_owner_token_id(e: &Env, owner: Address, index: TokenId) -> TokenId {
-        non_fungible::enumerable::storage::get_owner_token_id(e, &owner, index)
+        Enumerable::get_owner_token_id(e, &owner, index)
     }
 
     fn get_token_id(e: &Env, index: TokenId) -> TokenId {
-        non_fungible::enumerable::storage::get_token_id(e, index)
+        Enumerable::get_token_id(e, index)
     }
 }
 
 #[contractimpl]
 impl ExampleContract {
     pub fn mint(e: &Env, to: Address) -> TokenId {
-        non_fungible::enumerable::storage::sequential_mint(e, &to)
+        Enumerable::sequential_mint(e, &to)
     }
 
     pub fn burn(e: &Env, from: Address, token_id: TokenId) {
-        non_fungible::enumerable::storage::sequential_burn(e, &from, token_id);
+        Enumerable::sequential_burn(e, &from, token_id);
     }
 }
 
@@ -125,10 +124,10 @@ impl ExampleContract {
 // #[contractimpl]
 // impl NonFungibleBurnable for ExampleContract {
 //     fn burn(e: &Env, from: Address, token_id: TokenId) {
-//         non_fungible::burnable::burn(e, &from, token_id);
+//        Base::burn(e, &from, token_id);
 //     }
 
 //     fn burn_from(e: &Env, spender: Address, from: Address, token_id: TokenId)
-// {         non_fungible::burnable::burn_from(e, &spender, &from, token_id);
+// {        Base::burn_from(e, &spender, &from, token_id);
 //     }
 // }
--- packages/tokens/non-fungible/src/overrides.rs
+++ packages/tokens/non-fungible/src/overrides.rs
@@ -1,34 +1,25 @@
 use soroban_sdk::{Address, Env, String};
 
-use crate::TokenId;
+use crate::{Balance, TokenId};
 
 /// Based on the Extension, some default behavior of [`crate::NonFungibleToken`]
 /// might have to be overridden. This is a helper trait that allows us this
 /// override mechanism.
 pub trait ContractOverrides {
-    fn owner_of(e: &Env, token_id: TokenId) -> Address;
-    fn token_uri(e: &Env, token_id: TokenId) -> String;
-    fn transfer(e: &Env, from: Address, to: Address, token_id: TokenId);
-    fn transfer_from(e: &Env, spender: Address, from: Address, to: Address, token_id: TokenId);
-    fn approve(
-        e: &Env,
-        approver: Address,
-        approved: Address,
-        token_id: TokenId,
-        live_until_ledger: u32,
-    );
-}
-
-/// Default marker type
-pub struct Base;
-
-impl ContractOverrides for Base {
-    fn owner_of(e: &Env, token_id: TokenId) -> Address {
-        crate::owner_of(e, token_id)
+    fn balance(e: &Env, owner: Address) -> Balance {
+        Base::balance(e, &owner)
     }
 
-    fn token_uri(e: &Env, token_id: TokenId) -> String {
-        crate::token_uri(e, token_id)
+    fn owner_of(e: &Env, token_id: TokenId) -> Address {
+        Base::owner_of(e, token_id)
+    }
+
+    fn transfer(e: &Env, from: Address, to: Address, token_id: TokenId) {
+        Base::transfer(e, &from, &to, token_id);
+    }
+
+    fn transfer_from(e: &Env, spender: Address, from: Address, to: Address, token_id: TokenId) {
+        Base::transfer_from(e, &spender, &from, &to, token_id);
     }
 
     fn approve(
@@ -38,14 +29,36 @@ impl ContractOverrides for Base {
         token_id: TokenId,
         live_until_ledger: u32,
     ) {
-        crate::approve(e, &approver, &approved, token_id, live_until_ledger);
+        Base::approve(e, &approver, &approved, token_id, live_until_ledger);
     }
 
-    fn transfer(e: &Env, from: Address, to: Address, token_id: TokenId) {
-        crate::transfer(e, &from, &to, token_id);
+    fn approve_for_all(e: &Env, owner: Address, operator: Address, live_until_ledger: u32) {
+        Base::approve_for_all(e, &owner, &operator, live_until_ledger);
     }
 
-    fn transfer_from(e: &Env, spender: Address, from: Address, to: Address, token_id: TokenId) {
-        crate::transfer_from(e, &spender, &from, &to, token_id);
+    fn get_approved(e: &Env, token_id: TokenId) -> Option<Address> {
+        Base::get_approved(e, token_id)
+    }
+
+    fn is_approved_for_all(e: &Env, owner: Address, operator: Address) -> bool {
+        Base::is_approved_for_all(e, &owner, &operator)
+    }
+
+    fn name(e: &Env) -> String {
+        Base::name(e)
+    }
+
+    fn symbol(e: &Env) -> String {
+        Base::symbol(e)
+    }
+
+    fn token_uri(e: &Env, token_id: TokenId) -> String {
+        Base::token_uri(e, token_id)
     }
 }
+
+/// Default marker type
+pub struct Base;
+
+// No override required for the `Base` contract type.
+impl ContractOverrides for Base {}
