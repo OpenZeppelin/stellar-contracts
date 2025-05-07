03117782409b62e5b315b1a35fdffa410a0c4ede
brozorec <9572072+brozorec@users.noreply.github.com>
2025-05-07 15:29:19 +0200

    add soroban-test-helpers

--- packages/tokens/fungible/src/utils/sac_admin/test.rs
+++ packages/tokens/fungible/src/utils/sac_admin/test.rs
@@ -8,21 +8,35 @@ use soroban_sdk::{
     token::StellarAssetClient,
     Address, Env, IntoVal,
 };
+use soroban_test_helpers;
 
-use crate::sac_admin::storage::set_sac_address;
+use crate::sac_admin::storage::{get_sac_address, set_sac_address};
 
 use super::storage::set_admin;
 
 #[contract]
 struct MockContract;
 
-#[test]
-fn test_sac_set_address() {}
+#[soroban_test_helpers::test]
+fn test_sac_set_address(e: Env, sac: Address) {
+    let new_admin = e.register(MockContract, ());
 
-#[test]
-fn test_sac_set_admin() {
-    let e = Env::default();
-    let issuer = Address::generate(&e);
+    e.as_contract(&new_admin, || {
+        set_sac_address(&e, &sac);
+        assert_eq!(get_sac_address(&e), sac);
+    });
+}
+
+#[soroban_test_helpers::test]
+#[should_panic(expected = "Error(Contract, #209)")]
+fn test_sac_get_address_fails(e: Env) {
+    let new_admin = e.register(MockContract, ());
+
+    e.as_contract(&new_admin, || get_sac_address(&e));
+}
+
+#[soroban_test_helpers::test]
+fn test_sac_set_admin(e: Env, issuer: Address) {
     let new_admin = e.register(MockContract, ());
 
     // Deploy the Stellar Asset Contract
@@ -30,7 +44,7 @@ fn test_sac_set_admin() {
     let sac_client = StellarAssetClient::new(&e, &sac.address());
     assert_eq!(sac_client.admin(), issuer);
 
-    e.mock_auths(&[MockAuth {
+    env.mock_auths(&[MockAuth {
         // issuer authorizes
         address: &issuer,
         invoke: &MockAuthInvoke {
