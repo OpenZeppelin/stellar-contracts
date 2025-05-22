034413d00e87f5acde16a9013fcf9a738cae42ce
brozorec <9572072+brozorec@users.noreply.github.com>
2025-05-22 11:47:36 +0200

    fmt

--- packages/contract-utils/crypto/src/keccak.rs
+++ packages/contract-utils/crypto/src/keccak.rs
@@ -39,9 +39,8 @@ mod test {
 
     use proptest::prelude::*;
 
-    use crate::hasher::BuildHasher;
-
     use super::*;
+    use crate::hasher::BuildHasher;
 
     fn non_empty_u8_vec_strategy() -> impl Strategy<Value = Vec<u8>> {
         prop::collection::vec(any::<u8>(), 1..ProptestConfig::default().max_default_size_range)
