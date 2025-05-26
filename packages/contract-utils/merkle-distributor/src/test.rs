83187be2df6cdac4f41142c5400f1b678498682d
brozorec <9572072+brozorec@users.noreply.github.com>
2025-05-26 15:00:36 +0200

    add tests

--- packages/contract-utils/merkle-distributor/src/test.rs
+++ packages/contract-utils/merkle-distributor/src/test.rs
@@ -5,6 +5,7 @@ extern crate std;
 use hex_literal::hex;
 use soroban_sdk::{contract, contracttype, vec, xdr::ToXdr, Address, BytesN, Env};
 use stellar_crypto::{hasher::Hasher, sha256::Sha256};
+use stellar_event_assertion::EventAssertion;
 
 use crate::MerkleDistributor;
 
@@ -23,7 +24,7 @@ struct LeafData {
 }
 
 #[test]
-fn claim_valid_leaf() {
+fn test_valid_merkle_proof_succeeds() {
     let e = Env::default();
     let address = e.register(MockContract, ());
 
@@ -34,12 +35,10 @@ fn claim_valid_leaf() {
 
     let receiver =
         Address::from_str(&e, "CAASCQKVVBSLREPEUGPOTQZ4BC2NDBY2MW7B2LGIGFUPIY4Z3XUZRVTX");
-    let amount = 100;
-    let data = LeafData { index: 3, address: receiver.clone(), amount };
+    let data = LeafData { index: 3, address: receiver, amount: 100 };
     let mut hasher = Sha256::new(&e);
     hasher.update(data.to_xdr(&e));
     let leaf = hasher.finalize();
-
     let proof = vec![
         &e,
         Bytes32::from_array(
@@ -54,6 +53,83 @@ fn claim_valid_leaf() {
 
     e.as_contract(&address, || {
         Distributor::set_root(&e, root);
+        Distributor::verify_and_set_claimed(&e, leaf.clone(), proof);
+        assert!(Distributor::is_claimed(&e, leaf));
+    });
+}
+
+#[test]
+#[should_panic(expected = "Error(Contract, #1200)")]
+fn test_root_not_set_fails() {
+    let e = Env::default();
+    let address = e.register(MockContract, ());
+
+    e.as_contract(&address, || {
+        Distributor::get_root(&e);
+    });
+}
+
+#[test]
+#[should_panic(expected = "Error(Contract, #1201)")]
+fn test_set_root_twice_fails() {
+    let e = Env::default();
+    let address = e.register(MockContract, ());
+    let root = Bytes32::from_array(&e, &[1u8; 32]);
+
+    e.as_contract(&address, || {
+        Distributor::set_root(&e, root.clone());
+        Distributor::set_root(&e, root);
+    });
+}
+
+#[test]
+#[should_panic(expected = "Error(Contract, #1202)")]
+fn test_claim_already_claimed_leaf_fails() {
+    let e = Env::default();
+    let address = e.register(MockContract, ());
+    let root = Bytes32::from_array(&e, &[2u8; 32]);
+    let leaf = Bytes32::from_array(&e, &[3u8; 32]);
+    let proof = vec![&e, Bytes32::from_array(&e, &[4u8; 32])];
+
+    e.as_contract(&address, || {
+        Distributor::set_root(&e, root);
+        Distributor::set_claimed(&e, leaf.clone());
         Distributor::verify_and_set_claimed(&e, leaf, proof);
+    });
+}
+
+#[test]
+#[should_panic(expected = "Error(Contract, #1203)")]
+fn test_verify_with_invalid_proof_fails() {
+    let e = Env::default();
+    let address = e.register(MockContract, ());
+    let root = Bytes32::from_array(&e, &[5u8; 32]);
+    let leaf = Bytes32::from_array(&e, &[6u8; 32]);
+    let invalid_proof = vec![&e, Bytes32::from_array(&e, &[7u8; 32])];
+
+    e.as_contract(&address, || {
+        Distributor::set_root(&e, root);
+        Distributor::verify_and_set_claimed(&e, leaf, invalid_proof);
+    });
+}
+
+#[test]
+fn test_successful_claim_emits_events() {
+    let e = Env::default();
+    let address = e.register(MockContract, ());
+
+    let root = Bytes32::from_array(&e, &[8u8; 32]);
+    let leaf = Bytes32::from_array(&e, &[9u8; 32]);
+
+    e.as_contract(&address, || {
+        // Set root and verify event
+        Distributor::set_root(&e, root);
+        let assert = EventAssertion::new(&e, address.clone());
+        assert.assert_event_count(1);
+
+        // Set claimed and verify event
+        Distributor::set_claimed(&e, leaf);
+        let assert = EventAssertion::new(&e, address.clone());
+        assert.assert_event_count(2);
     });
 }
