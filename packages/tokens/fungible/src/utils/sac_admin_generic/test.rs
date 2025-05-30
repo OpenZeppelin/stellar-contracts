a0614bff2e5b45ba94ed02973d46bda3fb300687
brozorec <9572072+brozorec@users.noreply.github.com>
2025-05-30 14:18:45 +0200

    fix example

--- packages/tokens/fungible/src/utils/sac_admin_generic/test.rs
+++ packages/tokens/fungible/src/utils/sac_admin_generic/test.rs
@@ -4,22 +4,36 @@ extern crate std;
 
 use soroban_sdk::{
     auth::{Context, ContractContext},
+    contract,
     testutils::{Address as _, MockAuth, MockAuthInvoke},
     Address, BytesN, Env, Symbol, Val,
 };
 
-use super::{extract_context, get_sac_address, set_sac_address, SacFn};
+use soroban_test_helpers;
 
-fn setup_env() -> (Env, Address) {
-    let e = Env::default();
-    let sac = Address::generate(&e);
-    set_sac_address(&e, &sac);
-    (e, sac)
+use crate::sac_admin_generic::storage::SACAdminGenericDataKey;
+
+use super::{extract_sac_contract_context, get_sac_address, set_sac_address, SacFn};
+
+#[contract]
+struct MockContract;
+
+#[soroban_test_helpers::test]
+fn test_set_and_get_sac_address(e: Env, sac: Address) {
+    let new_admin = e.register(MockContract, ());
+
+    e.as_contract(&new_admin, || {
+        set_sac_address(&e, &sac);
+        let sac_addr: Address = e.storage().instance().get(&SACAdminGenericDataKey::Sac).unwrap();
+        assert_eq!(get_sac_address(&e), sac);
+        assert_eq!(sac_addr, sac);
+    });
 }
 
-#[test]
-fn test_set_and_get_sac_address() {
-    let (e, sac) = setup_env();
-    let stored = get_sac_address(&e);
-    assert_eq!(stored, sac);
+#[soroban_test_helpers::test]
+#[should_panic(expected = "Error(Contract, #109)")]
+fn test_sac_get_address_fails(e: Env) {
+    let new_admin = e.register(MockContract, ());
+
+    e.as_contract(&new_admin, || get_sac_address(&e));
 }
