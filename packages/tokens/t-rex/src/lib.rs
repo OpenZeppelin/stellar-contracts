b96985e77a94bfe9acc09e6f0783876d9ef30eb3
Ozgun Ozerk <ozgun@subspace.network>
2025-06-22 12:34:09 +0300

    remove unnecessary file and include claim topics

--- packages/tokens/t-rex/src/lib.rs
+++ packages/tokens/t-rex/src/lib.rs
@@ -1,10 +1,12 @@
 #![no_std]
 
+mod claim_topics;
 mod claims_registry;
 mod compliance;
 mod t_rex;
 mod token_registry;
 
+pub use claim_topics::*;
 pub use claims_registry::*;
 pub use compliance::*;
 pub use t_rex::TRexToken;
