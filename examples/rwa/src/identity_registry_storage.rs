80faa8f065be70edac8a8e93c3b38b50cbf47bf2
brozorec <9572072+brozorec@users.noreply.github.com>
2025-08-07 13:35:01 +0200

    example

--- examples/rwa/src/identity_registry_storage.rs
+++ examples/rwa/src/identity_registry_storage.rs
@@ -1,9 +1,9 @@
 use soroban_sdk::{contract, contractimpl, Address, Env, Vec};
 use stellar_tokens::rwa::{
     identity_storage_registry::{
-        self as irs, CountryProfile, CountryProfileManager, IdentityRegistryStorage,
+        self as identity_storage, CountryProfile, CountryProfileManager, IdentityRegistryStorage,
     },
-    utils::token_binder::TokenBinder,
+    utils::token_binder::{self as binder, TokenBinder},
 };
 
 #[contract]
@@ -16,16 +16,20 @@ impl IdentityRegistryContract {
 
 #[contractimpl]
 impl TokenBinder for IdentityRegistryContract {
-    fn linked_tokens(_e: &Env) -> Vec<Address> {
-        unimplemented!()
+    fn linked_tokens(e: &Env) -> Vec<Address> {
+        binder::linked_tokens(e)
     }
 
-    fn bind_token(_e: &Env, _token: Address, _operator: Address) {
-        unimplemented!()
+    fn bind_token(e: &Env, token: Address, operator: Address) {
+        // TODO: access control operator
+        operator.require_auth();
+        binder::bind_token(e, &token);
     }
 
-    fn unbind_token(_e: &Env, _token: Address, _operator: Address) {
-        unimplemented!()
+    fn unbind_token(e: &Env, token: Address, operator: Address) {
+        // TODO: access control operator
+        operator.require_auth();
+        binder::unbind_token(e, &token);
     }
 }
 
@@ -40,19 +44,19 @@ impl IdentityRegistryStorage for IdentityRegistryContract {
         initial_profiles: Vec<CountryProfile>,
         _operator: Address,
     ) {
-        irs::add_identity(e, &account, &identity, &initial_profiles);
+        identity_storage::add_identity(e, &account, &identity, &initial_profiles);
     }
 
     fn modify_identity(e: &Env, account: Address, new_identity: Address, _operator: Address) {
-        irs::modify_identity(e, &account, &new_identity);
+        identity_storage::modify_identity(e, &account, &new_identity);
     }
 
     fn remove_identity(e: &Env, account: Address, _operator: Address) {
-        irs::remove_identity(e, &account);
+        identity_storage::remove_identity(e, &account);
     }
 
     fn stored_identity(e: &Env, account: Address) -> Address {
-        irs::get_identity(e, &account)
+        identity_storage::get_identity(e, &account)
     }
 }
 
@@ -64,7 +68,7 @@ impl CountryProfileManager for IdentityRegistryContract {
         profiles: Vec<CountryProfile>,
         _operator: Address,
     ) {
-        irs::add_country_profiles(e, &account, &profiles);
+        identity_storage::add_country_profiles(e, &account, &profiles);
     }
 
     fn modify_country_profile(
@@ -74,22 +78,22 @@ impl CountryProfileManager for IdentityRegistryContract {
         profile: CountryProfile,
         _operator: Address,
     ) {
-        irs::modify_country_profile(e, &account, index, &profile);
+        identity_storage::modify_country_profile(e, &account, index, &profile);
     }
 
     fn delete_country_profile(e: &Env, account: Address, index: u32, _operator: Address) {
-        irs::delete_country_profile(e, &account, index);
+        identity_storage::delete_country_profile(e, &account, index);
     }
 
     fn get_country_profile(e: &Env, account: Address, index: u32) -> CountryProfile {
-        irs::get_country_profile(e, &account, index)
+        identity_storage::get_country_profile(e, &account, index)
     }
 
     fn get_country_profiles(e: &Env, account: Address) -> Vec<CountryProfile> {
-        irs::get_country_profiles(e, &account)
+        identity_storage::get_country_profiles(e, &account)
     }
 
     fn get_country_profile_count(e: &Env, account: Address) -> u32 {
-        irs::get_country_profile_count(e, &account)
+        identity_storage::get_country_profile_count(e, &account)
     }
 }
