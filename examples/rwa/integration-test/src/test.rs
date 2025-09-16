eb2c2a7f10ba5cfcdc9010c46373c15530ee7253
Ozgun Ozerk <ozgun@subspace.network>
2025-09-16 13:40:22 +0300

    working with upgradeable wasm import

--- examples/rwa/integration-test/src/test.rs
+++ examples/rwa/integration-test/src/test.rs
@@ -5,8 +5,12 @@ use soroban_sdk::contractimport;
 // use soroban_sdk::{testutils::Address as _, Address, Env, String, Vec};
 use stellar_tokens::rwa::identity_registry_storage::{CountryCode, CountryData};
 
-mod claim_issuer {
-    soroban_sdk::contractimport!(file = "./testdata/claim_issuer_example.wasm");
+// mod claim_issuer {
+//     soroban_sdk::contractimport!(file = "./testdata/claim_issuer_example.wasm");
+// }
+
+mod contract_v1 {
+    soroban_sdk::contractimport!(file = "./testdata/upgradeable_v1_example.wasm");
 }
 
 #[test]
