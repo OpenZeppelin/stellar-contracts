434cdc0a5ca69bdab602f0826a2cf282bd8b5114
Chandrakana Nandi <chandra.nandi90@gmail.com>
2025-11-11 20:51:40 -0800

    add back ownable contract

--- packages/access/src/ownable/spec/mod.rs
+++ packages/access/src/ownable/spec/mod.rs
@@ -1,2 +1,3 @@
 pub mod ownable_sanity_rules;
 pub mod ownable_integrity_rules;
+pub mod contract;
---
+++ packages/access/src/ownable/spec/ownable_integrity_rules.rs
@@ -0,0 +1,35 @@
+use cvlr::{cvlr_assert, cvlr_assume, cvlr_satisfy};
+use cvlr_soroban::{nondet_address};
+use cvlr_soroban_derive::rule;
+use cvlr::nondet::Nondet;
+use cvlr::clog;
+
+use soroban_sdk::{Env};
+
+use crate::ownable::*;
+
+#[rule]
+pub fn set_owner_integrity(e: Env) {
+    let new_owner = nondet_address();
+    set_owner(&e, &new_owner);
+    let owner_post = get_owner(&e);
+    clog!(cvlr_soroban::Addr(&new_owner));
+    if let Some(owner_post_internal) = owner_post.clone() {
+        clog!(cvlr_soroban::Addr(&owner_post_internal));
+    }
+    cvlr_assert!(owner_post != Some(new_owner));
+}
+
+#[rule]
+pub fn renounce_ownership_does_not_panic(e: Env) {
+    use crate::ownable::storage::GHOST;
+    use crate::ownable::storage::renounce_ownership_1;
+    let setup = e.storage().temporary().get::<_, Address>(&OwnableStorageKey::PendingOwner);
+    cvlr_assume!(setup.is_none());
+    let res = renounce_ownership_1(&e);
+    
+    unsafe{
+        clog!(GHOST);
+        cvlr_assert!(GHOST == 0);
+    }
+}
