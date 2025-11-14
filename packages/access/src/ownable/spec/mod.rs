b55506c1a7c5e280b020ea7d55289c611fc22097
Chandrakana Nandi <chandra.nandi90@gmail.com>
2025-11-14 10:48:23 -0800

    wip

--- packages/access/src/ownable/spec/mod.rs
+++ packages/access/src/ownable/spec/mod.rs
@@ -1,4 +1,4 @@
 pub mod ownable_sanity_rules;
 pub mod ownable_integrity_rules;
-pub mod panic_rules;
+pub mod ownable_panic_rules;
 pub mod contract;
--- packages/contract-utils/src/crypto/mod.rs
+++ packages/contract-utils/src/crypto/mod.rs
@@ -7,3 +7,6 @@ pub mod sha256;
 
 #[cfg(test)]
 mod test;
+
+#[cfg(feature = "certora")]
+pub mod spec;
