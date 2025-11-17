8f293e7ec0bd69191bcdf9b5a5d2937358519029
raz-certora <raz@certora.com>
2025-11-17 17:38:36 +0200

    access_control harness changes.

--- certora/access_control/src/access_control_contract.rs
+++ certora/access_control/src/access_control_contract.rs
@@ -10,6 +10,35 @@ impl FVHarnessAccessControlContract {
      pub fn __constructor(e: &Env, admin: Address) {
         set_admin(e, &admin);
     }
+    #[only_admin]
+    pub fn admin_function(e: &Env) {
+    }
+
+    #[has_role(caller, "role1")]
+    pub fn role1_function(e: &Env, caller: Address) {
+    }
+
+    #[only_role(caller, "role1")]
+    pub fn role1_authorized_function(e: &Env, caller: Address) {
+    }
+
+    #[has_any_role(caller, ["role1", "role2"])]
+    pub fn role1_or_role2_function(e: &Env, caller: Address) {
+    }
+
+    #[only_any_role(caller, ["role1", "role2"])]
+    pub fn role1_or_role2_authorized_function(e: &Env, caller: Address) {
+    }
+
+    #[has_role(caller1, "role1")]
+    #[has_role(caller2, "role2")]
+    pub fn role1_and_role2_function(e: &Env, caller1: Address, caller2: Address) {
+    }
+
+    #[has_role(caller, "role1")]
+    #[has_role(caller, "role2")]
+    pub fn role1_and_role2_on_same_addressfunction(e: &Env, caller: Address) {
+    }
 }
 
 #[default_impl]
