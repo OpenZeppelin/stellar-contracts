011c0c9ba46869e851b481c414529e9d9d03a365
raz-certora <raz@certora.com>
2025-11-22 20:32:14 +0200

    ownable_non_panics done.

--- packages/access/src/ownable/specs/ownable_non_panics.rs
+++ packages/access/src/ownable/specs/ownable_non_panics.rs
@@ -1,23 +1,82 @@
 use cvlr::{cvlr_assert, cvlr_assume};
 use cvlr_soroban::{nondet_address, is_auth};
 use cvlr_soroban_derive::rule;
-
+use cvlr::nondet::Nondet;
 
 use soroban_sdk::{Address, Env};
 
 use crate::ownable::specs::ownable_contract::OwnableContract;
 use crate::ownable::*;
 
-use crate::{ownable::OwnableStorageKey};
+use crate::{ownable::{OwnableStorageKey, specs::helper::get_pending_owner}};
 // These rules require the prover arg "prover_args": ["-trapAsAssert true"] to consider also panicking paths.
-
-// TODO:
-// non-panic for transfer_ownership
-// non-panic for accept_ownership
 
 #[rule]
 // requires
-// pending_owner is an address
+// storage setup
+// owner exists
+// owner auth
+// if there is a pending owner they are the same
+// live until ledger is appropriate
+// status: verified
+pub fn transfer_ownership_non_panic(e: Env) {
+    let address1 = nondet_address();
+    e.storage().temporary().set(&OwnableStorageKey::PendingOwner, &address1);
+
+    let address2 = nondet_address();
+    e.storage().instance().set(&OwnableStorageKey::Owner, &address2);
+    
+    let new_owner = nondet_address().clone();
+    let live_until_ledger = u32::nondet();
+
+    let owner = OwnableContract::get_owner(&e);
+    cvlr_assume!(owner.is_some());
+    if let Some(owner_internal) = owner.clone() {
+        cvlr_assume!(is_auth(owner_internal));
+    }
+
+    let pending_owner = get_pending_owner(&e);
+    if let Some(pending_owner_internal) = pending_owner.clone() {
+        cvlr_assume!(pending_owner_internal == new_owner);
+    }
+
+    if live_until_ledger == 0 {
+        cvlr_assume!(pending_owner.is_some());
+    }
+    else {
+        cvlr_assume!(live_until_ledger >= e.ledger().sequence());
+        cvlr_assume!(live_until_ledger <= e.ledger().max_live_until_ledger());
+    }
+
+    OwnableContract::transfer_ownership(&e, new_owner, live_until_ledger);
+    cvlr_assert!(true);
+}
+
+#[rule]
+// requires
+// storage setup
+// pending_owner is some
+// pending_owner auth
+// status: verified
+pub fn accept_ownership_non_panic(e: Env) {
+    let address1 = nondet_address();
+    e.storage().temporary().set(&OwnableStorageKey::PendingOwner, &address1);
+
+    let address2 = nondet_address();
+    e.storage().instance().set(&OwnableStorageKey::Owner, &address2);
+
+    let pending_owner = get_pending_owner(&e);
+    cvlr_assume!(pending_owner.is_some());
+    if let Some(pending_owner_internal) = pending_owner.clone() {
+        cvlr_assume!(is_auth(pending_owner_internal));
+    }
+    OwnableContract::accept_ownership(&e);
+    cvlr_assert!(true);
+}
+
+#[rule]
+// requires
+// storage setup
 // pending_owner is none
 // status: verified
 pub fn renounce_ownership_non_panic(e: Env) {
@@ -31,7 +90,7 @@ pub fn renounce_ownership_non_panic(e: Env) {
     // require_storage_tag(OwnableStorageKey::PendingOwner.into_val(&e), 77);
 
     let pending_owner = e.storage().temporary().get::<_, Address>(&OwnableStorageKey::PendingOwner);
-    // cvlr_assume!(pending_owner.is_none());
+    cvlr_assume!(pending_owner.is_none());
     let owner = OwnableContract::get_owner(&e);
     cvlr_assume!(owner.is_some());
     if let Some(owner_internal) = owner.clone() {
@@ -43,3 +102,24 @@ pub fn renounce_ownership_non_panic(e: Env) {
 }
 
 // non-panic for owner_restricted_function
+#[rule]
+// requires
+// storage setup
+// owner exists
+// owner auth
+// status: verified
+pub fn owner_restricted_function_non_panic(e: Env) {
+    let address1 = nondet_address();
+    e.storage().temporary().set(&OwnableStorageKey::PendingOwner, &address1);
+
+    let address2 = nondet_address();
+    e.storage().instance().set(&OwnableStorageKey::Owner, &address2);
+
+    let owner = OwnableContract::get_owner(&e);
+    cvlr_assume!(owner.is_some());
+    if let Some(owner_internal) = owner.clone() {
+        cvlr_assume!(is_auth(owner_internal));
+    }
+    OwnableContract::owner_restricted_function(&e);
+    cvlr_assert!(true);
+}
