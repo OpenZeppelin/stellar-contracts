2198b2e00f73e110579c871532882c9b8f707187
raz-certora <raz@certora.com>
2025-11-25 17:36:29 +0200

    start of work on policies

--- packages/accounts/src/policies/specs/mod.rs
+++ packages/accounts/src/policies/specs/mod.rs
@@ -1,3 +1,5 @@
-pub mod policies_simple_threshold_sanity;
-pub mod policies_weighted_threshold_sanity;
-pub mod policies_spending_limit_sanity;
+pub mod simple_threshold_sanity;
+pub mod simple_threshold_contract;
+pub mod simple_threshold_contract_sanity;
+pub mod weighted_threshold_sanity;
+pub mod spending_limit_sanity;
---
+++ packages/accounts/src/policies/specs/simple_threshold_contract_sanity.rs
@@ -0,0 +1,79 @@
+use core::task::Context;
+
+use cvlr::{
+    cvlr_assert,
+    nondet::{self, Nondet},
+    cvlr_satisfy,
+};
+use cvlr_soroban::{nondet_address, nondet_vec};
+use cvlr_soroban_derive::rule;
+use soroban_sdk::{Address, Env, Vec};
+
+use crate::{
+    policies::{Policy, simple_threshold::SimpleThresholdAccountParams, specs::simple_threshold_contract::SimpleThresholdPolicy},
+    smart_account::{ContextRule, Signer},
+};
+
+#[rule]
+pub fn get_simple_threshold_sanity(e: Env) {
+    let ctx_rule_id: u32 = u32::nondet();
+    let account_id = nondet_address();
+    let _ = SimpleThresholdPolicy::get_threshold(&e, ctx_rule_id, account_id);
+    cvlr_satisfy!(true);
+}
+
+#[rule]
+pub fn can_enforce_simple_threshold_sanity(e: Env, context: soroban_sdk::auth::Context) {
+    let auth_signers: Vec<Signer> = nondet_vec();
+    let ctx_rule: ContextRule = ContextRule::nondet();
+    let account: Address = nondet_address();
+    let _ = SimpleThresholdPolicy::can_enforce(
+        &e,
+        context,
+        auth_signers,
+        ctx_rule,
+        account,
+    );
+    cvlr_satisfy!(true);
+}
+
+#[rule]
+pub fn enforce_simple_threshold_sanity(e: Env, context: soroban_sdk::auth::Context) {
+    let auth_signers = nondet_vec();
+    let ctx_rule: ContextRule = ContextRule::nondet();
+    let account: Address = nondet_address();
+    let _ = SimpleThresholdPolicy::enforce(
+        &e,
+        context,
+        auth_signers,
+        ctx_rule,
+        account,
+    );
+    cvlr_satisfy!(true);
+}
+
+#[rule]
+pub fn set_simple_threshold_sanity(e: Env) {
+    let threshold: u32 = u32::nondet();
+    let ctx_rule: ContextRule = ContextRule::nondet();
+    let account_id = nondet_address();
+    SimpleThresholdPolicy::set_threshold(&e, threshold, ctx_rule, account_id);
+    cvlr_satisfy!(true);
+}
+
+#[rule]
+pub fn install_simple_threshold_sanity(e: Env) {
+    let params = SimpleThresholdAccountParams::nondet();
+    let ctx_rule: ContextRule = ContextRule::nondet();
+    let account_id = nondet_address();
+    SimpleThresholdPolicy::install(&e, params, ctx_rule, account_id);
+    cvlr_satisfy!(true);
+}
+
+#[rule]
+pub fn uninstall_simple_threshold_sanity(e: Env) {
+    let ctx_rule: ContextRule = ContextRule::nondet();
+    let account_id = nondet_address();
+    SimpleThresholdPolicy::uninstall(&e, ctx_rule, account_id);
+    cvlr_satisfy!(true);
+}
