6a1d90a210a47ba40aab1e21215dacdca4f9df3c
raz-certora <raz@certora.com>
2025-11-25 15:55:34 +0200

    update math rule

--- packages/contract-utils/src/math/specs/math_integrity.rs
+++ packages/contract-utils/src/math/specs/math_integrity.rs
@@ -8,18 +8,10 @@ use crate::math::soroban_fixed_point::SorobanFixedPoint;
 
 // todo: handle the muldiv function directly, need support for nondet_rounding.
 
-pub fn abs(x: i128) -> i128 {
-    if x < 0 {
-        -x
-    } else {
-        x
-    }
-}
-
 #[rule]
 // result is at most expected
-// status: first assert verified
-// second assert violated - seems spurious
+// status: first assert verified, second violated spurious
+// 
 pub fn fixed_mul_floor_integrity(e: &Env) {
     let x = i128::nondet();
     clog!(x);
@@ -31,17 +23,17 @@ pub fn fixed_mul_floor_integrity(e: &Env) {
     clog!(result);
     let expected_result = x * y / z;
     clog!(expected_result);
-    let max_rounding_error = abs(1/z);
+    let max_rounding_error = 1;
     clog!(max_rounding_error);
-    clog!(expected_result - max_rounding_error);
+    let lower_bound = expected_result.checked_sub(max_rounding_error).unwrap();
+    clog!(lower_bound);
     cvlr_assert!(result <= expected_result);
-    cvlr_assert!(result >= expected_result - max_rounding_error);
+    cvlr_assert!(result >= lower_bound);
 }
 
 #[rule]
 // result is at least expected
-// status: first assert verified
-// second assert violated - seems spurious
+// status: first assert verified, second violated spurious
 pub fn fixed_mul_ceil_integrity(e: &Env) {
     let x = i128::nondet();
     clog!(x);
@@ -53,9 +45,10 @@ pub fn fixed_mul_ceil_integrity(e: &Env) {
     clog!(result);
     let expected_result = x * y / z;
     clog!(expected_result);
-    let max_rounding_error = abs(1/z);
+    let max_rounding_error = 1;
     clog!(max_rounding_error);
-    clog!(expected_result + max_rounding_error);
+    let upper_bound = expected_result.checked_add(max_rounding_error).unwrap();
+    clog!(upper_bound);
     cvlr_assert!(result >= expected_result);
-    cvlr_assert!(result <= expected_result + max_rounding_error);
+    cvlr_assert!(result <= upper_bound);
 }
