3832dcaa65cbea5ef31f26f1aaabdb8c8de4bca7
raz-certora <raz@certora.com>
2025-12-04 10:24:30 +0200

    fungible non-panics

--- packages/tokens/src/fungible/specs/fungible_non_panics.rs
+++ packages/tokens/src/fungible/specs/fungible_non_panics.rs
@@ -5,15 +5,27 @@ use cvlr_soroban_derive::rule;
 use soroban_sdk::{Address, Env};
 use crate::fungible::FungibleToken;
 use crate::fungible::Base;
-
+use crate::fungible::storage::{StorageKey, AllowanceKey, AllowanceData};
 // These rules require the prover arg "prover_args": ["-trapAsAssert true"] to consider also panicking paths.
+
+pub fn storage_setup_balance(e: Env, account: Address) {
+    let balance:i128 = nondet();
+    e.storage().persistent().set(&StorageKey::Balance(account), &balance);
+}
+
+pub fn storage_setup_allowance(e: Env, owner: Address, spender: Address) {
+    let amount:i128 = nondet();
+    let live_until_ledger:u32 = nondet();
+    let allowance = AllowanceData { amount, live_until_ledger };
+    e.storage().temporary().set(&StorageKey::Allowance(AllowanceKey { owner, spender }), &allowance);
+}
 
 #[rule]
 // requires
 // from auth
 // from has enough balance
 // amount >= 0
-// status: wip
+// status: violated - problem with storage and nondet?
 pub fn transfer_non_panic(e: Env) {
     let to: Address = nondet_address();
     clog!(cvlr_soroban::Addr(&to));
@@ -22,6 +34,7 @@ pub fn transfer_non_panic(e: Env) {
     let amount:i128 = nondet();
     clog!(amount);
     cvlr_assume!(is_auth(from.clone()));
+    storage_setup_balance(e.clone(), from.clone());
     let from_balance = Base::balance(&e, &from);
     clog!(from_balance);
     cvlr_assume!(from_balance >= amount);
@@ -31,12 +44,32 @@ pub fn transfer_non_panic(e: Env) {
 }
 
 #[rule]
+// sanity
+// status: verified
+pub fn transfer_non_panic_sanity(e: Env) {
+    let to: Address = nondet_address();
+    clog!(cvlr_soroban::Addr(&to));
+    let from = nondet_address();
+    clog!(cvlr_soroban::Addr(&from));
+    let amount:i128 = nondet();
+    clog!(amount);
+    cvlr_assume!(is_auth(from.clone()));
+    storage_setup_balance(e.clone(), from.clone());
+    let from_balance = Base::balance(&e, &from);
+    clog!(from_balance);
+    cvlr_assume!(from_balance >= amount);
+    cvlr_assume!(amount >= 0);
+    Base::transfer(&e, &from, &to, amount);
+    cvlr_satisfy!(true);
+}
+
+#[rule]
 // requires
 // spender auth
 // from has enough allowance
 // spender has enough allowance
 // amount >= 0
-// status: wip
+// status: violated - problem with storage and nondet?
 pub fn transfer_from_non_panic(e: Env) {
     let to: Address = nondet_address();
     clog!(cvlr_soroban::Addr(&to));
@@ -47,9 +80,11 @@ pub fn transfer_from_non_panic(e: Env) {
     let amount:i128 = nondet();
     clog!(amount);
     cvlr_assume!(is_auth(spender.clone()));
+    storage_setup_balance(e.clone(), from.clone());
     let balance_from = Base::balance(&e, &from);
     clog!(balance_from);
     cvlr_assume!(balance_from >= amount);
+    storage_setup_allowance(e.clone(), from.clone(), spender.clone());
     let allowance_spender = Base::allowance(&e, &from, &spender);
     clog!(allowance_spender);
     cvlr_assume!(allowance_spender >= amount);
@@ -58,4 +93,82 @@ pub fn transfer_from_non_panic(e: Env) {
     cvlr_assert!(true);
 }
 
-// approve non_panic
+#[rule]
+// sanity
+// status: verified
+pub fn transfer_from_non_panic_sanity(e: Env) {
+    let to: Address = nondet_address();
+    clog!(cvlr_soroban::Addr(&to));
+    let from = nondet_address();
+    clog!(cvlr_soroban::Addr(&from));
+    let spender = nondet_address();
+    clog!(cvlr_soroban::Addr(&spender));
+    let amount:i128 = nondet();
+    clog!(amount);
+    cvlr_assume!(is_auth(spender.clone()));
+    storage_setup_balance(e.clone(), from.clone());
+    let balance_from = Base::balance(&e, &from);
+    clog!(balance_from);
+    cvlr_assume!(balance_from >= amount);
+    storage_setup_allowance(e.clone(), from.clone(), spender.clone());
+    let allowance_spender = Base::allowance(&e, &from, &spender);
+    clog!(allowance_spender);
+    cvlr_assume!(allowance_spender >= amount);
+    cvlr_assume!(amount >= 0);
+    Base::transfer_from(&e, &spender, &from, &to, amount);
+    cvlr_satisfy!(true);
+}
+
+#[rule]
+// requires
+// owner auth
+// amount >= 0
+// valid live until ledger
+// status: verified
+pub fn approve_non_panic(e: Env) {
+    let owner = nondet_address();
+    clog!(cvlr_soroban::Addr(&owner));
+    let spender = nondet_address();
+    clog!(cvlr_soroban::Addr(&spender));
+    let amount:i128 = nondet();
+    clog!(amount);
+    let live_until_ledger = nondet();
+    clog!(live_until_ledger);
+    cvlr_assume!(is_auth(owner.clone()));
+    cvlr_assume!(amount >= 0);
+    let current_ledger = e.ledger().sequence();
+    let max_live_until_ledger = e.ledger().max_live_until_ledger();
+    let non_zero_amount = amount > 0;
+    let ledger_more_than_max = live_until_ledger > max_live_until_ledger;
+    let ledger_less_than_current = live_until_ledger < current_ledger;
+    cvlr_assume!(!ledger_more_than_max);
+    cvlr_assume!(!(non_zero_amount && ledger_less_than_current));
+    Base::approve(&e, &owner, &spender, amount, live_until_ledger);
+    cvlr_assert!(true);
+}
+
+
+#[rule]
+// sanity
+// status: verified
+pub fn approve_non_panic_sanity(e: Env) {
+    let owner = nondet_address();
+    clog!(cvlr_soroban::Addr(&owner));
+    let spender = nondet_address();
+    clog!(cvlr_soroban::Addr(&spender));
+    let amount:i128 = nondet();
+    clog!(amount);
+    let live_until_ledger = nondet();
+    clog!(live_until_ledger);
+    cvlr_assume!(is_auth(owner.clone()));
+    cvlr_assume!(amount >= 0);
+    let current_ledger = e.ledger().sequence();
+    let max_live_until_ledger = e.ledger().max_live_until_ledger();
+    let non_zero_amount = amount > 0;
+    let ledger_more_than_max = live_until_ledger > max_live_until_ledger;
+    let ledger_less_than_current = live_until_ledger < current_ledger;
+    cvlr_assume!(!ledger_more_than_max);
+    cvlr_assume!(!(non_zero_amount && ledger_less_than_current));
+    Base::approve(&e, &owner, &spender, amount, live_until_ledger);
+    cvlr_satisfy!(true);
+}
