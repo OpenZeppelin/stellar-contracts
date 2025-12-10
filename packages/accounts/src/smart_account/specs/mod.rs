c4b813a1bc10c93116b0afe219fdd8986a317b21
raz-certora <raz@certora.com>
2025-12-10 15:04:29 +0200

    merge fix

--- packages/accounts/src/smart_account/specs/mod.rs
+++ packages/accounts/src/smart_account/specs/mod.rs
@@ -3,6 +3,4 @@ pub mod smart_account_sanity;
 pub mod smart_account_contract;
 pub mod smart_account_integrity;
 pub mod policy;
-pub mod smart_account_contract;
-pub mod smart_account_sanity;
 pub mod verifer;
