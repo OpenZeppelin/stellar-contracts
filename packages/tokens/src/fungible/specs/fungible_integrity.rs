26822fd46231cac161a4c51027cfe7ec4063ed05
raz-certora <raz@certora.com>
2025-12-14 12:26:21 +0200

    status

--- packages/tokens/src/fungible/specs/fungible_integrity.rs
+++ packages/tokens/src/fungible/specs/fungible_integrity.rs
@@ -94,8 +94,9 @@ pub fn transfer_from_integrity_3(e: Env) {
 
 #[rule]
 // transfer_from changes allowance accordingly
-// status: violation: https://prover.certora.com/output/33158/0180ad7c7e534d6cbc950a393201775c
-// not sure if this is even true because `allowance` has an additional check that `allowance_data` does not.
+// status: violation - spurious
+// https://prover.certora.com/output/33158/0180ad7c7e534d6cbc950a393201775c
+// Chandra: not sure if this is even true because `allowance` has an additional check that `allowance_data` does not.
 // see changes made below.
 // Raz: to me this looks spurious.
 pub fn transfer_from_integrity_4(e: Env) {
