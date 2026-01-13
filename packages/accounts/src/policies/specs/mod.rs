c4d141b42fa3c37eb74349a3b1c46166750a7a1a
raz-certora <raz@certora.com>
2026-01-13 10:59:11 +0200

    cleanup

--- packages/accounts/src/policies/specs/mod.rs
+++ packages/accounts/src/policies/specs/mod.rs
@@ -1,17 +1,13 @@
 pub mod simple_threshold_contract;
-pub mod simple_threshold_contract_sanity;
 pub mod simple_threshold_integrity;
 pub mod simple_threshold_non_panics;
 pub mod simple_threshold_panics;
 pub mod simple_threshold_invariants;
 
 pub mod weighted_threshold_contract;
-pub mod weighted_threshold_contract_sanity;
 pub mod weighted_threshold_integrity;
-pub mod weighted_threshold_non_panics;
 pub mod weighted_threshold_panics;
 pub mod weighted_threshold_invariants;
 
 pub mod spending_limit_contract;
-pub mod spending_limit_contract_sanity;
 pub mod spending_limit_integrity;
