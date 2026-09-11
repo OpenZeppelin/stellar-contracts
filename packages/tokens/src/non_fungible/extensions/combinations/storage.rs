36dec1dc5cc1be580e3aec6d74259d6adc7cbc79
Ozgun Ozerk <ozgunozerk.elo@gmail.com>
2026-09-11 11:23:54 +0300

    codecov

--- packages/tokens/src/non_fungible/extensions/combinations/storage.rs
+++ packages/tokens/src/non_fungible/extensions/combinations/storage.rs
@@ -44,44 +44,32 @@ impl BurnableOverrides for EnumerableVotes {
 impl RoyaltySupport for EnumerableVotes {}
 
 impl EnumerableVotes {
-    /// refer to [`Enumerable::sequential_mint`] and [`transfer_voting_units`]
-    /// for the inline documentation.
     pub fn sequential_mint(e: &Env, to: &Address) -> u32 {
         let token_id = Enumerable::sequential_mint(e, to);
         transfer_voting_units(e, None, Some(to), 1);
         token_id
     }
 
-    /// refer to [`Enumerable::non_sequential_mint`] and
-    /// [`transfer_voting_units`] for the inline documentation.
     pub fn non_sequential_mint(e: &Env, to: &Address, token_id: u32) {
         Enumerable::non_sequential_mint(e, to, token_id);
         transfer_voting_units(e, None, Some(to), 1);
     }
 
-    /// refer to [`Enumerable::transfer`] and [`transfer_voting_units`] for
-    /// the inline documentation.
     pub fn transfer(e: &Env, from: &Address, to: &Address, token_id: u32) {
         Enumerable::transfer(e, from, to, token_id);
         transfer_voting_units(e, Some(from), Some(to), 1);
     }
 
-    /// refer to [`Enumerable::transfer_from`] and [`transfer_voting_units`]
-    /// for the inline documentation.
     pub fn transfer_from(e: &Env, spender: &Address, from: &Address, to: &Address, token_id: u32) {
         Enumerable::transfer_from(e, spender, from, to, token_id);
         transfer_voting_units(e, Some(from), Some(to), 1);
     }
 
-    /// refer to [`Enumerable::burn`] and [`transfer_voting_units`] for the
-    /// inline documentation.
     pub fn burn(e: &Env, from: &Address, token_id: u32) {
         Enumerable::burn(e, from, token_id);
         transfer_voting_units(e, Some(from), None, 1);
     }
 
-    /// refer to [`Enumerable::burn_from`] and [`transfer_voting_units`] for
-    /// the inline documentation.
     pub fn burn_from(e: &Env, spender: &Address, from: &Address, token_id: u32) {
         Enumerable::burn_from(e, spender, from, token_id);
         transfer_voting_units(e, Some(from), None, 1);
@@ -143,40 +131,27 @@ impl BurnableOverrides for ConsecutiveVotes {
 impl RoyaltySupport for ConsecutiveVotes {}
 
 impl ConsecutiveVotes {
-    /// Creates a batch of `amount` tokens for `to` and adds the matching
-    /// amount of voting units in a single checkpoint update.
-    ///
-    /// refer to [`Consecutive::batch_mint`] and [`transfer_voting_units`] for
-    /// the inline documentation.
     pub fn batch_mint(e: &Env, to: &Address, amount: u32) -> u32 {
         let last_id = Consecutive::batch_mint(e, to, amount);
         transfer_voting_units(e, None, Some(to), u128::from(amount));
         last_id
     }
 
-    /// refer to [`Consecutive::transfer`] and [`transfer_voting_units`] for
-    /// the inline documentation.
     pub fn transfer(e: &Env, from: &Address, to: &Address, token_id: u32) {
         Consecutive::transfer(e, from, to, token_id);
         transfer_voting_units(e, Some(from), Some(to), 1);
     }
 
-    /// refer to [`Consecutive::transfer_from`] and [`transfer_voting_units`]
-    /// for the inline documentation.
     pub fn transfer_from(e: &Env, spender: &Address, from: &Address, to: &Address, token_id: u32) {
         Consecutive::transfer_from(e, spender, from, to, token_id);
         transfer_voting_units(e, Some(from), Some(to), 1);
     }
 
-    /// refer to [`Consecutive::burn`] and [`transfer_voting_units`] for the
-    /// inline documentation.
     pub fn burn(e: &Env, from: &Address, token_id: u32) {
         Consecutive::burn(e, from, token_id);
         transfer_voting_units(e, Some(from), None, 1);
     }
 
-    /// refer to [`Consecutive::burn_from`] and [`transfer_voting_units`] for
-    /// the inline documentation.
     pub fn burn_from(e: &Env, spender: &Address, from: &Address, token_id: u32) {
         Consecutive::burn_from(e, spender, from, token_id);
         transfer_voting_units(e, Some(from), None, 1);
